399b3f1ac4072fe173c7a6e1a5bb11b7a9b21d73
Erick Rosa <erickpmotta@gmail.com>
2021-12-27 18:41:54 -0300

    description change

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "Grupo de Estudos Maria Lacerda de Moura",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Grupo de Estudos Maria Lacerda de Moura surgiu no âmbito de outro projeto de extensão voltado para a educação popular, 'Filosofia como Prática', a partir da demanda por um maior conhecimento da tradição de pensamento libertário, isto é, pela necessidade daqueles que compõem este projeto conhecer melhor a história do anarquismo, seus princípios e suas reais possibilidades em articulação com o tempo presente. Entretanto, o grupo ultrapassou o âmbito de nossas rodas de auto-formação, tomando vida própria e agregando um público que não se restringe aos participantes do mencionado projeto. Trata-se de um grupo de estudos itinerante, voltado para prática real, que desde 2014, possui sessões quinzenais em vários locais, dentre eles: Universidades; escolas da rede pública; ocupações sem teto ou culturais; praças públicas; a Mangueira em articulação com o pré-vestibular comunitário da ADEP, a aldeia maracanã.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
